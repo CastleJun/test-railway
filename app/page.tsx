f77ea7c07bb36a730cc30a4c3430bd3dbe8d525a
SEONGJUN <62754759+CastleJun@users.noreply.github.com>
2025-01-12 04:20:32 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
-  return (
-    <main>
-      <div>Next.js on Railway</div>
-    </main>
-  );
-}
+
+import Link from 'next/link';
+
+const page = [
+    { id: '한글' },
+    { id: 'english' },
+];
+
+const Page = () => {
+    return (
+        <div style={{ padding: '40px', background: 'red' }}>
+            {page.map(({ id }) => {
+                return (
+                    <div key={id}>
+                        <Link href={`/${id}`}>{id}</Link>
+                        <br />
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default Page;
